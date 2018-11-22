592c89ac065b7ec97a2734d38d44d89fe776f65f
mchardex <mchardex@gmail.com>
2018-11-22 01:03:26 +0100

    Updates

---
+++ precache-manifest.a9496edf27d90c76c5c5fcc8154ac86b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dcc8f8cd605134658ce",
+    "url": "/currency-converter/static/js/runtime~main.1dcc8f8c.js"
+  },
+  {
+    "revision": "181cdf6a44e5e3f4b8b3",
+    "url": "/currency-converter/static/js/main.181cdf6a.chunk.js"
+  },
+  {
+    "revision": "fc38137728fd08781f19",
+    "url": "/currency-converter/static/js/1.fc381377.chunk.js"
+  },
+  {
+    "revision": "181cdf6a44e5e3f4b8b3",
+    "url": "/currency-converter/static/css/main.dc68315b.chunk.css"
+  },
+  {
+    "revision": "e16c68dabf1fd0a0a6c071000474c286",
+    "url": "/currency-converter/index.html"
+  }
+];
